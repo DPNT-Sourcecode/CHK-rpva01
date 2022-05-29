1b6e8a290482beaa51fafeca6a02536345e45ea8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-05-29 20:30:48 +0100

    Sun May 29 20:30:48 BST 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-quantities = {"A": 0, "B": 0, "C": 0, "D": 0}
 unit_cost = {"A": 50, "B": 30, "C":20, "D": 15}
 deals = {"A": (3, 130), "B": (2,45), "C": (1,20), "D": (1,15)}
 
 def checkout(skus):
+    
+    quantities = {"A": 0, "B": 0, "C": 0, "D": 0}
     
     for c in skus:
         if c in quantities:
@@ -18,4 +19,5 @@ def checkout(skus):
         total += (quantities[c]//deals[c][0])*deals[c][1] + (quantities[c]%deals[c][0])*unit_cost[c]
 
     return total
+
 
