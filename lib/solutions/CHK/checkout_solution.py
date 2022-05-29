9adcb0ace67b916de6f253bdd178e3758d7cfaa7
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-05-29 21:43:02 +0100

    Sun May 29 21:43:02 BST 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-unit_cost = {"A": 50, "B": 30, "C":20, "D": 15, "E": 40}
+unit_cost = {"A": 50, "B": 30, "C":20, "D": 15, "E": 40, "F": 1}
 multi_deals = {"A": [(5, 200), (3, 130)], "B": [(2,45)]}
-free_deals = [("E", 2 , "B", 1)]
+free_deals = [("E", 2 , "B", 1), ("F", 2, "F", 1)]
 
 def checkout(skus):
     
-    quantities = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
+    quantities = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0}
     
     for c in skus:
         if c in unit_cost and c in quantities:
@@ -23,13 +23,18 @@ def checkout(skus):
 
     for f in free_deals:
         if f[0] in quantities and f[2] in quantities:
-            quantities[f[2]] -= f[3]*(quantities[f[0]]//f[1])
-            quantities[f[2]] = max(0, quantities[f[2]])
+            if f[0] == f[2]:
+                x = quantities[f[2]]//(f[1]+f[3])
+                y = max(0,quantities[f[2]] - (f[1]+f[3])*x - f[1])
+                print(x,y)
+                quantities[f[2]] -= f[3]*x + y
+            else:
+                quantities[f[2]] -= f[3]*(quantities[f[0]]//f[1])
+                quantities[f[2]] = max(0, quantities[f[2]])
     
     for c in quantities:
         X = multi_deals.get(c, [])
         for x in X:
-            print(c, quantities[c])
             total += x[1]*(quantities[c]//x[0])
             quantities[c] -= x[0]*(quantities[c]//x[0])
 
