9ff7ffe1d6374939be01e07c64db626d6720b81e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-05-29 21:22:49 +0100

    Sun May 29 21:22:49 BST 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,21 +3,36 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-unit_cost = {"A": 50, "B": 30, "C":20, "D": 15}
-deals = {"A": (3, 130), "B": (2,45), "C": (1,20), "D": (1,15)}
+unit_cost = {"A": 50, "B": 30, "C":20, "D": 15, "E": 40}
+multi_deals = {"A": [(5, 200), (3, 130)], "B": [(2,45)]}
+free_deals = [("E", 2 , "B", 1)]
 
 def checkout(skus):
     
-    quantities = {"A": 0, "B": 0, "C": 0, "D": 0}
+    quantities = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
     
     for c in skus:
-        if c in quantities:
+        if c in unit_cost and c in quantities:
             quantities[c] += 1
+        elif c in unit_cost:
+            quantities[c] = 0
         else:
             return -1
     
     total = 0
+
+    for f in free_deals:
+        if f[0] in quantities and f[2] in quantities:
+            quantities[f[2]] -= f[3]*(quantities[f[0]]//f[1])
+            quantities[f[2]] = max(0, quantities[f[2]])
+    
     for c in quantities:
-        total += (quantities[c]//deals[c][0])*deals[c][1] + (quantities[c]%deals[c][0])*unit_cost[c]
+        X = multi_deals.get(c, [])
+        for x in X:
+            print(c, quantities[c])
+            total += x[1]*(quantities[c]//x[0])
+            quantities[c] -= x[0]*(quantities[c]//x[0])
+
+        total += quantities[c]*unit_cost[c]
 
     return total
