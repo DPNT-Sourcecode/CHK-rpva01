59c99356ced4d2bece6662e7aba84cb3aefef4d3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-05-30 08:12:54 +0100

    Mon May 30 08:12:54 BST 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@
 # skus = unicode string
 
 unit_cost = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, 
-            "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, 
+            "H": 10, "I": 35, "J": 60, "K": 70, "L": 90, "M": 15, "N": 40, 
             "O": 10, "P": 50, "Q": 30, "R": 50, "S": 20, "T": 20, "U": 40, 
             "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21}
 multi_deals = {"A": [(5, 200), (3, 130)], "B": [(2,45)], 
@@ -72,3 +72,4 @@ def checkout(skus):
         total += quantities[c]*unit_cost[c]
 
     return total
+
