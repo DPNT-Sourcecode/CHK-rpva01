1ce2c4bd6c8c0582ff35b67a46c1162808eb95d3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2022-05-30 08:10:03 +0100

    Mon May 30 08:10:03 BST 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,8 +5,8 @@
 
 unit_cost = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, 
             "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, 
-            "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, 
-            "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
+            "O": 10, "P": 50, "Q": 30, "R": 50, "S": 20, "T": 20, "U": 40, 
+            "V": 50, "W": 20, "X": 17, "Y": 20, "Z": 21}
 multi_deals = {"A": [(5, 200), (3, 130)], "B": [(2,45)], 
             "H": [(10, 80), (5, 45)], "K" : [(2,150)], 
             "P" : [(5,200)], "Q": [(3, 80)], "V": [(3,130), (2,90)]}
@@ -48,7 +48,10 @@ def checkout(skus):
     ord_joint_deals = dict(sorted(ord_joint_deals.items(), 
                         key= lambda item: item[1], reverse=True))
 
-    sum_ojds = joint_deals[0][2](sum(ord_joint_deals.values())//joint_deals[0][2])
+    for x in ord_joint_deals:
+        ord_joint_deals[x] = quantities[x]
+
+    sum_ojds = joint_deals[0][2]*(sum(ord_joint_deals.values())//joint_deals[0][2])
     subtotal = sum_ojds//joint_deals[0][2]
 
     for x in ord_joint_deals:
